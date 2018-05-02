52fe7caf1d1d6ae2e8c6ce025274d2841379c761
wswapped <wswapped@gmail.com>
2018-05-02 13:22:11 +0200

    conne

--- functions/conne.php
+++ functions/conne.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "teranoba_user";
+$password = "teranoba";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
